6c286d21154b99570a90048fce34d3e42a25e113
valentine1004 <valentine1004@ukr.net>
2021-06-04 19:01:21 +0300

    rbac init settings

---
+++ server.js
@@ -0,0 +1,34 @@
+﻿require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const errorHandler = require('_helpers/error-handler');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// api routes
+app.use('/users', require('./users/users.controller'));
+
+// global error handler
+app.use(errorHandler);
+
+// Connection URL
+const url = 'mongodb://localhost:27017/water_admin';
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, { useUnifiedTopology: true }, function(err, client) {
+    assert.equal(null, err);
+    client.close();
+});
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
---
+++ users/user.service.js
@@ -0,0 +1,41 @@
+﻿const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const Role = require('_helpers/role');
+
+// users hardcoded for simplicity, store in a db for production applications
+const users = [
+    { id: 1, email: 'admin@gmail.com', password: 'admin', firstName: 'Admin', lastName: 'User', role: Role.Admin },
+    { id: 2, email: 'user@gmail.com', password: 'user', firstName: 'Normal', lastName: 'User', role: Role.User }
+];
+
+module.exports = {
+    authenticate,
+    getAll,
+    getById
+};
+
+async function authenticate({ email, password }) {
+    const user = users.find(u => u.email === email && u.password === password);
+    if (user) {
+        const token = jwt.sign({ sub: user.id, role: user.role }, config.secret);
+        const { password, ...userWithoutPassword } = user;
+        return {
+            ...userWithoutPassword,
+            token
+        };
+    }
+}
+
+async function getAll() {
+    return users.map(u => {
+        const { password, ...userWithoutPassword } = u;
+        return userWithoutPassword;
+    });
+}
+
+async function getById(id) {
+    const user = users.find(u => u.id === parseInt(id));
+    if (!user) return;
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
+}
